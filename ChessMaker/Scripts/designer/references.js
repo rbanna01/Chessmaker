2c1f0326fa040e2896c98739c1a438c22f2159cb
FTWinston <ftwinston@gmail.com>
2014-08-12 01:19:53 +0100

    Basic bulk renaming now works, though it currently only uses numbers, rather than letters, and the example on the popup isn't updated

--- ChessMaker/Scripts/designer/references.js
+++ ChessMaker/Scripts/designer/references.js
@@ -8,7 +8,7 @@ function populateLinkData() {
 
         var dir = parts[1];
         var from = parts[0];
-        var to = document.getElementById(parts[2]); // the actual object, to facilitate finding it easily after renaming
+        var to = $('#' + parts[2]);
 
         if (from == null || to == null)
             continue;
@@ -34,13 +34,13 @@ function cellClicked() {
 function renameSelectedCell() {
     var newName = $('#txtName').val();
     var cell = $('#render path.cell.selected');
-    return doRenameCell(cell, newName);
+    return doRenameCell(cell, newName) != null;
 }
 
 function doRenameCell(cell, newName) {
     // is this name already used? abort if so
     if ($('#render path.cell#' + newName).length > 0)
-        return false;
+        return null;
 
     var oldName = cell.attr('id');
     cell.attr('id', newName);
@@ -55,11 +55,38 @@ function doRenameCell(cell, newName) {
     allLinkData[newName] = allLinkData[oldName];
     delete allLinkData[oldName];
 
-    return true;
+    return newName;
 }
 
 function performAutoRename() {
-    // wow, we don't actually have a way to navigate the JS tree. I guess we need markers!
+    var initialCell = $('#render path.cell.selected');
+    remClass(initialCell, 'selected');
+
+    var dir1 = $('#bulkDir1').val();
+    var dir2 = $('#bulkDir2').val();
+
+    var start1 = $('#bulkDir1Start').val();
+    var start2 = $('#bulkDir2Start').val();
+
+    var prefix = $('#bulkDirPrefix').val();
+    var middle = $('#bulkDirMiddle').val();
+    var suffix = $('#bulkDirSuffix').val();
+
+    renameRecursive(initialCell, dir1, dir2, 1, 1, prefix, middle, suffix);
+    remClass($('#render path.cell.renamed'), 'renamed');
+}
+
+function renameRecursive(cell, dir1, dir2, nextRef1, nextRef2, prefix, middle, suffix) {
+    var cellID = doRenameCell(cell, prefix + nextRef1 + middle + nextRef2 + suffix);
+    addClass(cell, 'renamed');
+
+    var dir1cell = allLinkData[cellID][dir1];
+    if (typeof dir1cell != 'undefined' && !hasClass(dir1cell[0], 'renamed'))
+        renameRecursive(dir1cell, dir1, dir2, nextRef1 + 1, nextRef2, prefix, middle, suffix);
+
+    var dir2cell = allLinkData[cellID][dir2];
+    if (typeof dir2cell != 'undefined' && !hasClass(dir2cell[0], 'renamed'))
+        renameRecursive(dir2cell, dir1, dir2, nextRef1, nextRef2 + 1, prefix, middle, suffix);
 }
 
 $(function () {
@@ -88,7 +115,6 @@ $(function () {
         buttons: {
             "OK": function () {
                 performAutoRename();
-                remClass($('#render path.cell'), 'selected');
                 $(this).dialog("close");
             },
             Cancel: function () {
