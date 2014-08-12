4610cbcc36c459634a4ca099346ad32a16a43c6e
FTWinston <ftwinston@gmail.com>
2014-08-12 10:27:41 +0100

    Bulk renaming updated to handle letters in names, and to update its example as changes are made

--- ChessMaker/Scripts/designer/references.js
+++ ChessMaker/Scripts/designer/references.js
@@ -65,28 +65,65 @@ function performAutoRename() {
     var dir1 = $('#bulkDir1').val();
     var dir2 = $('#bulkDir2').val();
 
+    var isChar1 = $('#bulkDir1Type').val() != '1';
+    var isChar2 = $('#bulkDir2Type').val() != '1';
+
     var start1 = $('#bulkDir1Start').val();
     var start2 = $('#bulkDir2Start').val();
+
+    if (isChar1) {
+        start1 = start1.charCodeAt(0);
+        if (isNaN(start1))
+            start1 = 'a'.charCodeAt(0);
+    }
+    else {
+        start1 = parseInt(start1);
+        if (isNaN(start1))
+            start1 = 1;
+    }
+
+    if (isChar2) {
+        start2 = start2.charCodeAt(0);
+        if (isNaN(start2))
+            start2 = 'a'.charCodeAt(0);
+    }
+    else {
+        start2 = parseInt(start2);
+        if (isNaN(start2))
+            start2 = 1;
+    }       
 
     var prefix = $('#bulkDirPrefix').val();
     var middle = $('#bulkDirMiddle').val();
     var suffix = $('#bulkDirSuffix').val();
 
-    renameRecursive(initialCell, dir1, dir2, 1, 1, prefix, middle, suffix);
+    renameRecursive(initialCell, dir1, dir2, start1, start2, isChar1, isChar2, prefix, middle, suffix);
     remClass($('#render path.cell.renamed'), 'renamed');
 }
 
-function renameRecursive(cell, dir1, dir2, nextRef1, nextRef2, prefix, middle, suffix) {
-    var cellID = doRenameCell(cell, prefix + nextRef1 + middle + nextRef2 + suffix);
+function renameRecursive(cell, dir1, dir2, nextRef1, nextRef2, isChar1, isChar2, prefix, middle, suffix) {
+    var reference = prefix + (isChar1 ? String.fromCharCode(nextRef1) : nextRef1) + middle + (isChar2 ? String.fromCharCode(nextRef2) : nextRef2) + suffix;
+    var cellID = doRenameCell(cell, reference);
     addClass(cell, 'renamed');
 
     var dir1cell = allLinkData[cellID][dir1];
     if (typeof dir1cell != 'undefined' && !hasClass(dir1cell[0], 'renamed'))
-        renameRecursive(dir1cell, dir1, dir2, nextRef1 + 1, nextRef2, prefix, middle, suffix);
+        renameRecursive(dir1cell, dir1, dir2, nextRef1 + 1, nextRef2, isChar1, isChar2, prefix, middle, suffix);
 
     var dir2cell = allLinkData[cellID][dir2];
     if (typeof dir2cell != 'undefined' && !hasClass(dir2cell[0], 'renamed'))
-        renameRecursive(dir2cell, dir1, dir2, nextRef1, nextRef2 + 1, prefix, middle, suffix);
+        renameRecursive(dir2cell, dir1, dir2, nextRef1, nextRef2 + 1, isChar1, isChar2, prefix, middle, suffix);
+}
+
+function updateExample() {
+    var prefix = $('#bulkDirPrefix').val();
+    var middle = $('#bulkDirMiddle').val();
+    var suffix = $('#bulkDirSuffix').val();
+
+    var part1 = $('#bulkDir1Start').val();
+    var part2 = $('#bulkDir2Start').val();
+
+    $('#bulkExample').text(prefix + part1 + middle + part2 + suffix);
 }
 
 $(function () {
@@ -139,6 +176,9 @@ $(function () {
     $('#bulkDir2Type').change(function () {
         $('#bulkDir2Start').val($(this).val());
     });
+
+    $('.updateExample').change(updateExample);
+    updateExample();
 
     populateLinkData();
 
