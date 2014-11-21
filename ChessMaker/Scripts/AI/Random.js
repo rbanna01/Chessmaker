dcaaedfcb5ed6c5bef361cc6954e772267c9dbff
FTWinston <ftwinston@gmail.com>
2014-11-21 10:42:53 +0000

    Added "Random capture" AI

--- ChessMaker/Scripts/AI/Random.js
+++ ChessMaker/Scripts/AI/Random.js
@@ -1,10 +1,11 @@
-﻿function AI_selectMove() {
+﻿// Picks from all available moves, completely randomly.
+function AI_selectMove() {
     var player = game.currentPlayer;
 
     var allMoves = [];
     for (var i = 0; i < player.piecesOnBoard.length; i++) {
         var piece = player.piecesOnBoard[i];
-        allMoves = allMoves.concat(piece.getPossibleMoves());
+        allMoves = allMoves.concat(piece.getPossibleMoves(game));
     }
 
     var index = Math.floor(Math.random() * allMoves.length);
---
+++ ChessMaker/Scripts/AI/RandomCapture.js
@@ -0,0 +1,26 @@
+﻿// Picks from all available CAPTURE moves, completely randomly.
+// If there are non, picks from all available moves, completely randomly.
+function AI_selectMove() {
+    var player = game.currentPlayer;
+
+    var allMoves = [];
+    for (var i = 0; i < player.piecesOnBoard.length; i++) {
+        var piece = player.piecesOnBoard[i];
+        allMoves = allMoves.concat(piece.getPossibleMoves(game));
+    }
+
+    var captures = [];
+    for (var i = 0; i < allMoves.length; i++) {
+        var move = allMoves[i];
+        if (move.isCapture())
+            captures.push(move);
+    }
+
+    if (captures.length > 0) {
+        var index = Math.floor(Math.random() * captures.length);
+        return captures[index];
+    }
+
+    var index = Math.floor(Math.random() * allMoves.length);
+    return allMoves[index];
+}
